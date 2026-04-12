bba51bb7ed3e79d5dba529dfce16302be95d6ed5
dxd <dxd3678@163.com>
2026-04-12 14:52:34 +0800

    python(dic): learn how to get item

---
+++ python/dic.py
@@ -0,0 +1,16 @@
+
+country = {
+    "name":"China",
+    "province": [
+        {"name": "Zhejiang"},
+        {"name":"Shanghai"}
+    ]
+}
+
+# Get all keys, values, or items:
+print(country.keys())
+print(country.values())
+print(country.items())
+
+print(country["name"])
+print(country["province"])
